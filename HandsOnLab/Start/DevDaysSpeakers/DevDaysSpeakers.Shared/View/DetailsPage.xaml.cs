b2a22e5cd9cb609747994b663fb337162818a615
Tom Soderling <tom.soderling@mcomposer.com>
2016-10-24 12:00:42 -0500

    Added Conginitve Services NuGet packages to UWP project

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/View/DetailsPage.xaml.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/View/DetailsPage.xaml.cs
@@ -79,21 +79,45 @@ namespace DevDaysSpeakers.View
                     buttonText = "Awww";
 
                 await DisplayAlert("Happiness Level", EmotionService.GetHappinessMessage(level), buttonText);
-
-
-
-                 //Display more emotion scores
-                //var scores = await EmotionService.GetAverageEmotionScoresAsync(this.speaker.Avatar);
-
-                //var buttonText = "YAY!";
-                //if (scores[0] < Config.HAPPINESS_LEVEL_THRESHOLD) // score[0] is happiness
-                //    buttonText = "Awww";
-
-                //await DisplayAlert("Emotions Analysis", EmotionService.GetEmotionsMessage(scores), buttonText);
             }
             catch (Exception faceException)
             {
                 await DisplayAlert("Oops!", faceException.Message, "Rats");
+
+
+                // 10/24/2016 - getting this exception on UWP each time:
+                //     Message: Exception of type 'Microsoft.ProjectOxford.Common.ClientException' was thrown.
+                // Stack Trace:
+                /*
+               at Microsoft.ProjectOxford.Common.ServiceClient.<SendAsync>d__22`2.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at Microsoft.ProjectOxford.Common.ServiceClient.<PostAsync>d__20`2.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at Microsoft.ProjectOxford.Emotion.EmotionServiceClient.<RecognizeAsync>d__6.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at Microsoft.ProjectOxford.Emotion.EmotionServiceClient.<RecognizeAsync>d__5.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()
+               at DevDaysSpeakers.EmotionService.<GetEmotionsAsync>d__0.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()
+               at DevDaysSpeakers.EmotionService.<GetAverageHappinessScoreAsync>d__1.MoveNext()
+            --- End of stack trace from previous location where exception was thrown ---
+               at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+               at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()
+               at DevDaysSpeakers.View.DetailsPage.<ButtonSpecial_Clicked>d__3.MoveNext()
+               */
             }
         }
     }
